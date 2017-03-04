67b97278759b4be20a6f152dcac97c232144effd
makotomotegi <makotomotegi@gmail.com>
2017-03-04 04:35:10 +0000

    temp

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+class PostsController < ApplicationController
+  # GET /posts/new
+  def new
+  end
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+  # GET /posts/:id
+    def show
+      @post = Post.find(params[:id])
+  end
+
+  private
+    def post_params
+    params.require(:post).permit(:title, :text)
+  end
+end
+
