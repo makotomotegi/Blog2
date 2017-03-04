1b642be40376770fb92fcee036e1ba192837f0da
makotomotegi <makotomotegi@gmail.com>
2017-03-04 07:04:15 +0000

    temp2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,27 @@
 class PostsController < ApplicationController
   # GET /posts/new
   def new
+    @post = Post.new
   end
   # POST /posts
   def create
     @post = Post.new(post_params)
-    @post.save
+
+    if @post.save
     redirect_to @post
+    else
+    render :new, status: :unprocessable_entity
+    end
+
   end
   # GET /posts/:id
     def show
       @post = Post.find(params[:id])
+  end
+
+  # GET /posts
+    def index
+      @posts = Post.all
   end
 
   private
