618e1b7acb645ebf4fcae46772aabfebafbc7d55
makotomotegi <makotomotegi@gmail.com>
2017-03-05 14:03:14 +0000

    temp6

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+  # POST /posts/:post_id/comments
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+
+    redirect_to post_path(@post)
+  end
+
+  # DELETE /posts/:post_id
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
